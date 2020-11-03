94da1ce6d2f5159ea040aa94e978b7b9d1b9ab53
BetweenWalls <56801533+BetweenWalls@users.noreply.github.com>
2020-11-02 19:51:34 -0600

    minor
    * corrected IAS breakpoints for claw-class weapons

--- data/functions/updating.js
+++ data/functions/updating.js
@@ -269,6 +269,10 @@ function updatePrimaryStats() {
 		weaponFrames += 1
 		var combined_ias = Math.min(175,Math.max(15,(100 + c.ias_skill + eIAS - weaponSpeedModifier)));
 		var frames_per_attack = Math.ceil((weaponFrames*256)/Math.floor(anim_speed * combined_ias / 100)) - 1;
+		if (weaponType == "claw") {
+			var frames_per_attack_alternate = Math.ceil(((weaponFrames+1)*256)/Math.floor(anim_speed * combined_ias / 100)) - 1;
+			frames_per_attack = (frames_per_attack + frames_per_attack_alternate) / 2
+		}
 		/*
 		// TODO: implement wereform IAS frame info
 		if (typeof(effects["Werewolf"]) != 'undefined' || typeof(effects["Werebear"]) != 'undefined') {	if (effects["Werewolf"].info.enabled == 1 || effects["Werebear"].info.enabled == 1) {
